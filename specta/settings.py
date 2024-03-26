56d7dac3e26ab56a630b5e2df693d61cbd346e57
Adil <mail2akadil@gmail.com>
2024-03-26 14:36:53 +0530

    Add Localhost in Allowed Host

--- specta/settings.py
+++ specta/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['15.207.84.64', '0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1', '15.207.84.64', '0.0.0.0']
 
 
 # Application definition
